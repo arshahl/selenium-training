599f35883e6ee38eda88d9e0bb815806e5411341
arshahl <arshalokanathan@gmail.com>
2025-08-14 13:02:04 +0530

    adding changes to data.py

--- data.py
+++ data.py
@@ -1,6 +1,9 @@
-str="selenium"
+def length():
+    str="selenium"
 
-length =0
-for i in str:
-    length=length+1
-print(length)
+    length =0
+    for i in str:
+        length=length+1
+    print(length)
+
+length()
